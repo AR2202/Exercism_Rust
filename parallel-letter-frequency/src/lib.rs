67f6294c7c37758a776e73b552ecc8db2681e8d7
Annika Rings <annika.rings@dpag.ox.ac.uk>
2021-03-18 21:31:53 +0000

    changed parallell letter frequency

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,34 +1,5 @@
-use rayon::prelude::*;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex, RwLock};
-
-pub fn frequency1(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    if input.is_empty() {
-        let res: HashMap<char, usize> = HashMap::new();
-        return res;
-    }
-    let chunk_size = (input.len() + worker_count - 1) / worker_count;
-
-    let hm = input
-        .par_chunks(chunk_size)
-        .map(|chunk| {
-            chunk
-                .iter()
-                .map(|s| {
-                    s.to_lowercase().chars().filter(|c| c.is_alphabetic()).fold(
-                        HashMap::new(),
-                        |mut acc, ch| {
-                            *acc.entry(ch).or_insert(0) += 1;
-                            acc
-                        },
-                    )
-                })
-                .fold(HashMap::new(), |acc, hm2| merge_hashmaps(acc, hm2))
-        })
-        .reduce(|| HashMap::new(), |acc, hm2| merge_hashmaps(acc, hm2));
-
-    hm
-}
+use std::sync::{Arc, RwLock};
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let mut hm: Arc<RwLock<HashMap<char, usize>>> = Arc::new(RwLock::new(HashMap::new()));
@@ -52,14 +23,4 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     }
     let result = (*hm.read().unwrap()).clone();
     result
-}
-fn merge_hashmaps(
-    mut hm1: HashMap<char, usize>,
-    hm2: HashMap<char, usize>,
-) -> HashMap<char, usize> {
-    for (k, v) in hm2.iter() {
-        let v_old = hm1.entry(*k).or_insert(0);
-        *v_old += v;
-    }
-    hm1
 }
