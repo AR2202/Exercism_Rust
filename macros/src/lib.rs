dde661f1aac128611f29e6da50657065e4d83c2e
Annika Rings <annika.rings@dpag.ox.ac.uk>
2021-04-21 21:25:55 +0100

    added macro exercise

---
+++ macros/src/lib.rs
@@ -0,0 +1,25 @@
+pub use ::std::collections::HashMap;
+#[macro_export]
+
+macro_rules! hashmap {
+    
+   ($ ($x:expr => $y:expr),*)=> {
+        {
+            let mut temp_hashmap = ::std::collections::HashMap::new();
+            $(
+                temp_hashmap.insert($x, $y);
+            )*
+            temp_hashmap
+        }
+    };
+    ($ ($x:expr => $y:expr,)*)=> {
+        {
+            let mut temp_hashmap = ::std::collections::HashMap::new();
+            $(
+                temp_hashmap.insert($x, $y);
+            )*
+            temp_hashmap
+        }
+    };
+    
+}
